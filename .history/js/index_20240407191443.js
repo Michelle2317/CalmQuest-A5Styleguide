432b04bb79b577c321b2a5291ba43e92f93bd75a
Michelle2317 <michelle388q@gmail.com>
2024-04-07 19:54:39 -0700

    remove extra folder

--- .history/js/index_20240407191443.js
+++
@@ -1,9 +0,0 @@
-var path = window.location.pathname
-// Get the <a> element corresponding to the current page and add the "active" class
-if (path === '/') {
-	document.getElementById('home').classList.add('active')
-} else if (path === '/design.html') {
-	document.getElementById('design').classList.add('active')
-} else if (path === '/references.html') {
-	document.getElementById('references').classList.add('active')
-}
--- .history/js/index_20240407191535.js
+++
@@ -1,8 +0,0 @@
-var path = window.location.pathname
-if (path === '/') {
-	document.getElementById('logo').classList.add('active')
-} else if (path === '/design.html') {
-	document.getElementById('design').classList.add('active')
-} else if (path === '/references.html') {
-	document.getElementById('references').classList.add('active')
-}
